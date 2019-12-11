b2db4b1135b112af9cce5a7eefcb25f5e7534f54
shutoskate <wizwizwayne@gmail.com>
2019-12-11 17:00:34 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user! #ヘルパーメソッド　コントローラーに設定して、ログイン済ユーザーのみにアクセスを許可する
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseで利用出来るパラメーターを設定する
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #上記のメソッドに名前の登録を追加
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 root to:"messages#index"  
+resources :users, only: [:edit, :update] #ユーザー編集の為のルート設定
 end
