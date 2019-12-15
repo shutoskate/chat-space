71e62109ad31e60ad859ca4540d88f0f0f982e2e
shutoskate <wizwizwayne@gmail.com>
2019-12-15 19:30:14 +0900

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params) #保存処理ができた場合、できなかった場合で分ける。処理が成功ならrootへ　失敗ならeditのビューへいく設定
+    if current_user.update(user_params) #保存処理ができた場合、できなかった場合で分ける。処理が成功ならrootへ失敗ならeditのビューへいく設定
       redirect_to root_path
     else
       render :edit
